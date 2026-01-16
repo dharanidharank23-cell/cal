969ff52d7fb57330cea3d2cf0dc0d346ed43fdc4
DharaniDaran123 <dharanirock83@gmail.com>
2026-01-16 21:56:42 +0530

    changed something

--- cal/src/main/java/com/example/cal/controller/calcontoller.java
+++ cal/src/main/java/com/example/cal/controller/calcontoller.java
@@ -1,4 +1,4 @@
-package com.example.cal.controller;
+package com.example.cal.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
