76c803096b61fe8a769ff74d6c238788a6a7051e
RANJITHESWARAN1995 <ranjithcivil228@gmail.com>
2026-01-16 20:41:39 +0530

    New file cal/src/main/java/com/example/cal/Controller/calController.java

---
+++ cal/src/main/java/com/example/cal/Controller/calController.java
@@ -0,0 +1,17 @@
+package com.example.cal.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cal")
+public class calController {
+
+
+@GetMapping("/add")
+    public double add(@RequestParam double a, @RequestParam double b) {
+        return a + b;
+    }
+}
