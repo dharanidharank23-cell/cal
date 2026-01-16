021558d26f3dfde40a680221a27d10657acf043f
RANJITHESWARAN1995 <ranjithcivil228@gmail.com>
2026-01-16 21:30:26 +0530

    add Implement addition endpoint in calcontoller and set server port in application.properties

---
+++ cal/src/main/java/com/example/cal/controller/calcontoller.java
@@ -0,0 +1,13 @@
+package com.example.cal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class calcontoller {
+
+    @GetMapping("/add")
+    public double add(@RequestParam double a, @RequestParam double b) {
+        return a + b;
+    }
+
+}
